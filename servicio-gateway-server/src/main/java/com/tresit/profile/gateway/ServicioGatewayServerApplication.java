408feb7b8adbb6c575ffb1df312f9d94ab0cc8c1
Jacob Vega <jacob.vega@gmail.com>
2024-10-07 09:46:06 -0300

    Primera versión del Gateway

---
+++ servicio-gateway-server/src/main/java/com/tresit/profile/gateway/ServicioGatewayServerApplication.java
@@ -0,0 +1,13 @@
+package com.tresit.profile.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioGatewayServerApplication.class, args);
+	}
+
+}
